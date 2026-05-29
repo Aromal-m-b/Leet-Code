8aec05098c10143f224d6ba64bd67aec1f52f566
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2026-05-29 09:31:53 +0530

    Time: 7 ms (30.84%), Space: 19.2 MB (87.82%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -2,9 +2,8 @@ class Solution:
     def minElement(self, nums: List[int]) -> int:
         val = 10 ** 4
         for i in range (len(nums)):
-            temp  = str(nums[i])
             res = 0
-            for c in temp:
+            for c in str(nums[i]):
                 res = res + int(c)
             val = min(res,val)
             if val == 1:
