247bfa1d39884146e10cd91ffae613ac276ef653
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2026-05-31 10:08:12 +0530

    Time: 69 ms (78.12%), Space: 34 MB (53.36%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,9 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for n in asteroids:
+            if mass >= n:
+                mass+=n
+            else:
+                return False
+        return True
