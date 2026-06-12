4b3da3dd2fd0d920344d04357008153ab8f86776
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2026-06-12 12:19:28 +0530

    Time: 674 ms (7.45%), Space: 124.4 MB (24.47%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -1,24 +1,23 @@
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        tree = {}
+        for edge in edges:
+            if edge[0] not in tree:
+                tree[edge[0]] = []
+            if edge[1] not in tree:
+                tree[edge[1]] = []
+            tree[edge[0]].append(edge[1])
+            tree[edge[1]].append(edge[0])
+        print(tree)
+    
+        dp = {}
 
-        n = len(edges) + 1
-        g = [[] for _ in range(n + 1)] # edges for each node
-        for u, v in edges:
-            if u < v:
-                g[u].append(v) # bidirected ? no, only small -> large
-            else:
-                g[v].append(u) # bidirected
-
-        d = [-1] * (n + 1) # depths
-        d[1] = 0  # depth at 1 starting
-        q = deque([1]) 
-        while q:
-            u = q.popleft()
-            for v in g[u]:
-                if d[v] < 0: # haven't checked depth
-                    d[v] = d[u] + 1 # use partenth depth + 1
-                    q.append(v) # queue to check childs later
-
-        m = max(d[1:]) # get the max depth for node 1 
-        MOD = 10**9 + 7 # constant
-        return pow(2, m - 1, MOD) # power
+        def depth(node:int,parent:int) -> int:
+            max_depth = 0
+            for child in tree[node]:
+                if child == parent:
+                    continue
+                max_depth = max(depth(child,node)+ 1,max_depth)
+            return max_depth
+        d = depth(1,-1)
+        return ((2**(d-1)) % ((10**9)+7))
